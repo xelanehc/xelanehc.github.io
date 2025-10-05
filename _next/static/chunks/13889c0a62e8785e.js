922697f683f9924f13b77a53faf47d785b2a8d61
kaisernarkotic <achen1452@gmail.com>
2025-10-04 21:32:15 -0700

    added index.html entry file for github pages

--- _next/static/chunks/13889c0a62e8785e.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/70fe4f40cedef2bf.js",
-  "static/chunks/turbopack-1aaf2a92ca377994.js"
-])
--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // If your repository name is not 'xelanehc.github.io', uncomment and set the correct path:
-  // basePath: '/your-repo-name',
+  /* config options here */
 };
 
 export default nextConfig;
