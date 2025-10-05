adb5b2cfc66b43b7667ca008c9ae8a292994d9c7
kaisernarkotic <achen1452@gmail.com>
2025-10-04 21:27:09 -0700

    add static files for github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // If your repository name is not 'xelanehc.github.io', uncomment and set the correct path:
+  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
